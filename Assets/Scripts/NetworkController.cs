685387d031af53f3d73fc18d506ce6461e583025
chouhan-mahendra <mahendra.chouhan@moonfroglabs.com>
2019-06-02 17:06:23 +0530

    Client network controller

---
+++ Assets/Scripts/NetworkController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using SocketIO;
+using UnityEngine;
+
+public class NetworkController : MonoBehaviour
+{
+    private SocketIOComponent socket;
+    // Start is called before the first frame update
+    void Start()
+    {
+        socket = GetComponent<SocketIOComponent>();
+        socket.On("open",OnConnected);
+        socket.On("init", OnInit);
+        socket.On("startGame", OnStartGame);
+        socket.On("playerConnected", OnPlayerConnected);
+        socket.On("playerDisconnected", OnPlayerDisconnected);
+    }
+
+    private void OnPlayerDisconnected(SocketIOEvent obj)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnPlayerConnected(SocketIOEvent obj)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnStartGame(SocketIOEvent obj)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnInit(SocketIOEvent obj)
+    {
+
+    }
+
+    private void OnConnected(SocketIOEvent obj)
+    {
+        Debug.Log("connected to server");
+    }
+}
